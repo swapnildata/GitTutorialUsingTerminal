f5f03a55b8848e32b67865f170cbf4f72aa95819
swapnil <swapnilpowar8251@gmail.com>
2025-03-14 17:31:45 +0530

    added cueb functionality

--- Maths.py
+++ Maths.py
@@ -3,5 +3,11 @@ x=2
 def square(y):
     return y*y
 
+def cube(y):
+    return y*y*y
+
 square_of_x=square(x)
 print(f"square_of_x is {square_of_x}")
+
+cube_of_x=cube(x)
+print(f"cube_of_x is {cube_of_x}")
